b98e4e9e37e9c2916c9a39cc8c95b93c3169dc7f
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-05-20 15:50:05 +0700

    add: no 1 kelar bosh

--- soal_1/inikaryakita.c
+++ soal_1/inikaryakita.c
@@ -38,6 +38,16 @@ static int hello_mkdir(const char *path, mode_t mode) {
     return 0;
 }
 
+static int hello_rmdir(const char *path) {
+    char fpath[1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    int res = rmdir(fpath);
+    if (res == -1) {
+        return -errno;
+    }
+    return 0;
+}
+
 static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
     char fpath[1000];
     sprintf(fpath,"%s%s",dirpath,path);
@@ -66,15 +76,13 @@ static int hello_rename(const char *from, const char *to, unsigned int flags) {
     char fromPath[1000], toPath[1000];
     sprintf(fromPath,"%s%s",dirpath,from);
     sprintf(toPath,"%s%s",dirpath,to);
-    int res;
 
     if (strstr(toPath, "/wm") != NULL) {
         int srcfd = open(fromPath, O_RDONLY);
         int destfd = open(toPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-        char weem[] = "inikaryakita.id";
         char command[1000];
-        sprintf(command, "convert -gravity south -font Arial /proc/%d/fd/%d -fill white -pointsize 40 -annotate +0+0 '%s' /proc/%d/fd/%d", getpid(), srcfd, weem, getpid(), destfd);
+        sprintf(command, "convert -gravity south -font Arial /proc/%d/fd/%d -fill white -pointsize 50 -annotate +0+0 '%s' /proc/%d/fd/%d", getpid(), srcfd, "inikaryakita.id", getpid(), destfd);
 
         int res = system(command);
         if (res == -1) {
@@ -92,17 +100,111 @@ static int hello_rename(const char *from, const char *to, unsigned int flags) {
             return -errno;
         }
     } else {
-        int res = rename(fromPath, toPath);
+        if (rename(fromPath, toPath) == -1) {
+            perror("gabisa move");
+            return -errno;
+        }
     } 
     // sprintf(command, "convert %s -font Arial -pointsize 20 -draw \"gravity south fill black text 0,12 'inikaryakita.id' fill white text 1,11 'inikaryakita.id'\" %s", frompath, temp);
     return 0;
 }
 
+static int hello_unlink(const char *path) {
+    char fpath[1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    int res = unlink(fpath);
+    if (res == -1) {
+        return -errno;
+    }
+    return 0;
+}
+
+static int hello_chmod(const char *path, mode_t mode) {
+    char fpath[1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    if(chmod(fpath, mode) < 0)
+        return -errno;
+    return 0;
+}
+
+static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi) {
+    char fpath[1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+
+    // Check if the file has a 'test' prefix
+    const char *filename = strrchr(path, '/');
+    if (filename == NULL) {
+        filename = path;
+    } else {
+        filename++;
+    }
+
+    if (strncmp(filename, "test", 4) == 0) {
+        // If the file has a 'test' prefix, reverse the words in the lines before output
+        FILE *file = fopen(fpath, "r");
+        if (file == NULL) {
+            return -errno;
+        }
+
+        char line[1024];
+        buf[0] = '\0';  // Initialize the buffer to an empty string
+        while (fgets(line, sizeof(line), file) != NULL) {
+            size_t len = strlen(line);
+            if (line[len - 1] == '\n') {
+                line[len - 1] = '\0';
+            }
+
+            // Reverse the characters in each word
+            char *start = line;
+            char *end = start + strlen(line) - 1;
+            while (end > start) {
+                char temp = *start;
+                *start = *end;
+                *end = temp;
+                start++;
+                end--;
+            }
+
+            // Check if the buffer is large enough
+            if (strlen(buf) + strlen(line) + 1 > size) {
+                fclose(file);
+                return -ENOMEM;
+            }
+
+            strcat(buf, line);
+            strcat(buf, "\n");
+        }
+
+        fclose(file);
+    } else {
+        // If the file doesn't have a 'test' prefix, output normally
+        int fd = open(fpath, O_RDONLY);
+ if (fd == -1) {
+            return -errno;
+        }
+
+        int res = pread(fd, buf, size, offset);
+        if (res == -1) {
+            res = -errno;
+        }
+
+        close(fd);
+        return res;
+    }
+
+    return strlen(buf);
+}
+
 static struct fuse_operations hello_oper = {
     .getattr    = hello_getattr,
     .mkdir      = hello_mkdir,
+    .rmdir      = hello_rmdir,
     .readdir    = hello_readdir,
     .rename     = hello_rename,
+    .chmod      = hello_chmod,
+    .read       = hello_read,
+    .unlink     = hello_unlink,
 };
 
 int main(int argc, char *argv[]) {
